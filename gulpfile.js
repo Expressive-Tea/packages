d039c5a36c63edb3d9cab4e04349cc0854991d90
Diego Resendez <diego.resendez@zero-oneit.com>
2023-01-13 01:30:25 -0600

    [ENHANCEMENTS] Code Improvements

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const tsconfigPath = path.join(__dirname, 'tools/gulp/tsconfig.json');
-
-require('ts-node').register({
-  project: tsconfigPath
-});
-
-require('./tools/gulp/gulpfile');
--- tools/gulp/setup.ts
+++
@@ -1,4 +0,0 @@
-import FileHelper from "../utilities/file.helper";
-
-export const PACKAGES_FOLDER = 'packages';
-export const PACKAGES_PATHS = FileHelper.getDirs(PACKAGES_FOLDER);
